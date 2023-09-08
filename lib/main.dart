afdf7a283d1294977006fa67a17aa378c6719a28
Samuel Rigaud <samuel.rigaud@etu.unilim.fr>
2023-09-08 16:12:58 +0200

    little changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,197 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        home: MyHomePage(),
+        debugShowCheckedModeBanner: false,
+      );
+}
 
-  // This widget is the root of your application.
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Emploi du temps',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('flutter_cached_pdfview Demo'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute<dynamic>(
+                builder: (_) => const PDFViewerFromUrl(
+                  url: 'http://edt-iut-info.unilim.fr/edt/A3/A3_S1.pdf',
+                ),
+              ),
+            ),
+            child: const Text('PDF From Url'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute<dynamic>(
+                builder: (_) => const PDFViewerCachedFromUrl(
+                  url: 'http://edt-iut-info.unilim.fr/edt/A3/A3_S1.pdf',
+                ),
+              ),
+            ),
+            child: const Text('Cashed PDF From Url'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute<dynamic>(
+                builder: (_) => PDFViewerFromAsset(
+                  pdfAssetPath: 'assets/pdf/file-example.pdf',
+                ),
+              ),
+            ),
+            child: const Text('PDF From Asset'),
+          ),
+        ],
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class PDFViewerFromUrl extends StatelessWidget {
+  const PDFViewerFromUrl({Key? key, required this.url}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final String url;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        
-        title: Text(widget.title),
+        title: const Text('PDF From Url'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: const PDF().fromUrl(
+        url,
+        placeholder: (double progress) => Center(child: Text('$progress %')),
+        errorWidget: (dynamic error) => Center(child: Text(error.toString())),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class PDFViewerCachedFromUrl extends StatelessWidget {
+  const PDFViewerCachedFromUrl({Key? key, required this.url}) : super(key: key);
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cached PDF From Url'),
+      ),
+      body: const PDF().cachedFromUrl(
+        url,
+        placeholder: (double progress) => Center(child: Text('$progress %')),
+        errorWidget: (dynamic error) => Center(child: Text(error.toString())),
+      ),
+    );
+  }
+}
+
+class PDFViewerFromAsset extends StatelessWidget {
+  PDFViewerFromAsset({Key? key, required this.pdfAssetPath}) : super(key: key);
+  final String pdfAssetPath;
+  final Completer<PDFViewController> _pdfViewController =
+      Completer<PDFViewController>();
+  final StreamController<String> _pageCountController =
+      StreamController<String>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PDF From Asset'),
+        actions: <Widget>[
+          StreamBuilder<String>(
+              stream: _pageCountController.stream,
+              builder: (_, AsyncSnapshot<String> snapshot) {
+                if (snapshot.hasData) {
+                  return Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.blue[900],
+                      ),
+                      child: Text(snapshot.data!),
+                    ),
+                  );
+                }
+                return const SizedBox();
+              }),
+        ],
+      ),
+      body: PDF(
+        enableSwipe: true,
+        swipeHorizontal: true,
+        autoSpacing: false,
+        pageFling: false,
+        onPageChanged: (int? current, int? total) =>
+            _pageCountController.add('${current! + 1} - $total'),
+        onViewCreated: (PDFViewController pdfViewController) async {
+          _pdfViewController.complete(pdfViewController);
+          final int currentPage = await pdfViewController.getCurrentPage() ?? 0;
+          final int? pageCount = await pdfViewController.getPageCount();
+          _pageCountController.add('${currentPage + 1} - $pageCount');
+        },
+      ).fromAsset(
+        pdfAssetPath,
+        errorWidget: (dynamic error) => Center(child: Text(error.toString())),
+      ),
+      floatingActionButton: FutureBuilder<PDFViewController>(
+        future: _pdfViewController.future,
+        builder: (_, AsyncSnapshot<PDFViewController> snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            return Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                FloatingActionButton(
+                  heroTag: '-',
+                  child: const Text('-'),
+                  onPressed: () async {
+                    final PDFViewController pdfController = snapshot.data!;
+                    final int currentPage =
+                        (await pdfController.getCurrentPage())! - 1;
+                    if (currentPage >= 0) {
+                      await pdfController.setPage(currentPage);
+                    }
+                  },
+                ),
+                FloatingActionButton(
+                  heroTag: '+',
+                  child: const Text('+'),
+                  onPressed: () async {
+                    final PDFViewController pdfController = snapshot.data!;
+                    final int currentPage =
+                        (await pdfController.getCurrentPage())! + 1;
+                    final int numberOfPages =
+                        await pdfController.getPageCount() ?? 0;
+                    if (numberOfPages > currentPage) {
+                      await pdfController.setPage(currentPage);
+                    }
+                  },
+                ),
+              ],
+            );
+          }
+          return const SizedBox();
+        },
       ),
     );
   }
